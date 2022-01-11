a983a35e57de2f914a66c18a0b02f11c5ea83aae
unknown <m.berkay.akar@gmail.com>
2022-01-11 23:51:35 +0300

    second commit

---
+++ EĞİTİM/2.) Mini Project (Course App)/WebApplication2/WebApplication2/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+            // CONSOLE UYGULAMALARINDAKi GİBİ BİR SÜREÇ MEVCUT BUNUN İÇERİSİNDE HOSTİNG NESNESİ oluşturularak Build edilip run edilmektedir. 
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
---
+++ EĞİTİM/2.) Mini Project (Course App)/WebApplication2/WebApplication2/Startup.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2
+{
+    public class Startup
+    {
+      
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Burada MVC PATERNİNİ EKLEYECEĞİZ . BU SEBEPLE MODÜLLER BURAYA EKLENECEKTİR. 
+            // MVC İLK OLARAK BURAYA EKLENMEKTEDİR. 
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // 2.0 da buraya kadar 
+            // yeterlidir. 
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
+        // 
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            #region Gelen Tüm İsteklere Hello Word gndermek 
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Berkay AKAR ILK DENEME!");
+            //});
+            #endregion
+
+            // MiddleWare olarak ekleme yapacağız ;
+
+            // app.UseMvcWithDefaultRoute(); defaultta var olan standart routing i alır controller / action / ? id gibi elle yapılandırmak için aşaüıdaki gibi ekleme yapılması yeterlidir.
+
+            app.UseMvc(routes => {
+                routes.MapRoute
+                (
+                    name: "default",
+                    template: "{controller}/{action}/{id?}"
+                    );
+            });
+
+
+            // ? koyarak isteğe bağlı bir parametre olarak belirlemiş olduk
+
+        }
+    }
+}
